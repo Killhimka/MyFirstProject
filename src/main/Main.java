04fb9c7ff839b9609e5007bb235cab85f49e4cb3
Killhimka <126574108+Killhimka@users.noreply.github.com>
2023-02-28 17:01:21 +0300

    1.1

---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Project");
+        System.out.println("Project");
+    }
+}
